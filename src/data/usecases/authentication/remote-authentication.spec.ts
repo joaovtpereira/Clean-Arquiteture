8fd2f29cd707d7f7392abf75d0cad9172f6285c6
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2023-07-11 07:13:39 -0300

    chore: add faker and paths directory

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,5 @@
 import { RemoteAuthentication } from "./remote-authentication";
-import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { HttpPostClientSpy } from "@/data/test/mock-http-client";
 
 type SutTypes = {
   sut: RemoteAuthentication;
