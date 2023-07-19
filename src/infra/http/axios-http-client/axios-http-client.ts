60c249ee19869bccc13eb46566af327c8fe68c25
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2023-07-19 17:46:49 -0300

    feat: add axios

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,5 +1,7 @@
 import { HttpPostParams } from "@/data/protocols/http";
-
+import axios from "axios";
 export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {}
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url);
+  }
 }
